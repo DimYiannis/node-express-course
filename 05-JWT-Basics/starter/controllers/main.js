c57d051e144bc158f9f6e10a16a4453003c63780
Yiannis Dim <johndim99@gmail.com>
2023-11-01 11:01:07 +0200

    1st commit

---
+++ 05-JWT-Basics/starter/controllers/main.js
@@ -0,0 +1,29 @@
+// check username, password in post(login) request
+// if exist create new JWT
+// send back to fron-end
+// setup authentication so only the request with JWT can access the dasboard
+
+
+const CustomAPIError = require('../errors/custom-error')
+
+const login = async (req,res) =>{
+const {username, password} = req.body
+// mongoose validations
+//Joi
+// check in the controller
+
+if(!username || !password){
+    throw new CustomAPIError('Please provide email and password' , 400)
+}
+
+    res.send('Fake Login/Register/Signup Route')
+}
+
+const dashboard = async (req,res) =>{
+    const luckyNumber = Math.floor(Math.random()*100)
+    res.status(200).json({msg:`Hello, John Doe`, secret:`Here is your authorized data, your lucky number is ${luckyNumber}`})
+}
+
+module.exports= {
+    login, dashboard
+}
