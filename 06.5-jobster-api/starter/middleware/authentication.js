83994468de6283d877a4e0d9cf491d864e1dac37
yiannis <107484245+DimYiannis@users.noreply.github.com>
2023-11-06 17:43:02 +0200

    2nd commit

--- 06.5-jobster-api/starter/middleware/authentication.js
+++ 06.5-jobster-api/starter/middleware/authentication.js
@@ -13,7 +13,9 @@ const auth = async (req, res, next) => {
   try {
     const payload = jwt.verify(token, process.env.JWT_SECRET)
     // attach the user to the job routes
-    req.user = { userId: payload.userId, name: payload.name }
+    const testUser = payload.userId === '65480bbe17857c09c46a1028';
+  
+    req.user = { userId: payload.userId,testUser }
     next()
   } catch (error) {
     throw new UnauthenticatedError('Authentication invalid')
