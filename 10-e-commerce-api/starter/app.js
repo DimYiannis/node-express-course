e954fb7f1a907bcd833a4c86bdd392ba5db109ad
yiannis <107484245+DimYiannis@users.noreply.github.com>
2023-11-18 21:33:06 +0200

    order structure

--- 10-e-commerce-api/starter/app.js
+++ 10-e-commerce-api/starter/app.js
@@ -8,7 +8,7 @@ const app = express();
 // rest of the packages
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
-const fileUpload = require('express-fileupload')
+const fileUpload = require("express-fileupload");
 
 //DB
 const connectDB = require("./db/connect");
@@ -18,6 +18,7 @@ const authRouter = require("./routes/authRoutes");
 const userRouter = require("./routes/userRoutes");
 const productRouter = require("./routes/productRoutes");
 const reviewRouter = require("./routes/reviewRoutes");
+const orderRouter = require("./routes/orderRoutes");
 
 //middleware
 const notFoundMiddleware = require("./middleware/not-found");
@@ -27,23 +28,24 @@ app.use(morgan("tiny"));
 app.use(express.json());
 app.use(cookieParser(process.env.JWT_SECRET));
 
-app.use(express.static('./public'))
-app.use(fileUpload())
+app.use(express.static("./public"));
+app.use(fileUpload());
 
 app.get("/", (req, res) => {
-  console.log(req.cookies)
+  console.log(req.cookies);
   res.send("e-commerce api");
 });
 
 app.get("/api/v1", (req, res) => {
-  console.log(req.signedCookies)
+  console.log(req.signedCookies);
   res.send("e-commerce api");
 });
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/products", productRouter);
-app.use('/api/v1/reviews', reviewRouter)
+app.use("/api/v1/reviews", reviewRouter);
+app.use("/api/v1/orders", orderRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
--- 10-e-commerce-api/starter/controllers/reviewController.js
+++ 10-e-commerce-api/starter/controllers/reviewController.js
@@ -31,7 +31,16 @@ const createReview = async (req, res) => {
 };
 
 const getAllReviews = async (req, res) => {
-  const reviews = await Review.find({});
+  const reviews = await Review.find({})
+    .populate({
+      path: "product",
+      select: "name company price ",
+    })
+    .populate({
+      path: "user",
+      select: "name ",
+    });
+
   res.status(StatusCodes.OK).json({ reviews, count: reviews.length });
 };
 
@@ -82,10 +91,19 @@ const deleteReview = async (req, res) => {
   res.status(StatusCodes.OK).json({ msg: "Success! Review removed" });
 };
 
+// alternative single product reviews
+const getSingleProductReviews = async (req, res) => {
+  const {id:productId} = req.params
+  const reviews = await Review.find({product:productId})
+  res.status(StatusCodes.OK).json({ reviews, count: reviews.length})
+};
+
+
 module.exports = {
   createReview,
   getAllReviews,
   getSingleReview,
   updateReview,
   deleteReview,
+  getSingleProductReviews,
 };
